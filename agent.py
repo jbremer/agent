e93969114542a5cf525fd5c928b251c90389bade
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-09-09 16:01:26 +0200

    initial agent with bare functionality

---
+++ agent.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# Copyright (C) 2010-2015 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import argparse
+import os
+import platform
+import subprocess
+import sys
+import tempfile
+import traceback
+import zipfile
+
+try:
+    from flask import Flask, request, jsonify
+except ImportError:
+    sys.exit("ERROR: Flask library is missing (`pip install flask`)")
+
+app = Flask("agent")
+
+def json_error(error_code, message):
+    r = jsonify(message=message, error_code=error_code)
+    r.status_code = error_code
+    return r
+
+def json_exception(message):
+    r = jsonify(message=message, error_code=500,
+                traceback=traceback.format_exc())
+    r.status_code = 500
+    return r
+
+def json_success(message, **kwargs):
+    return jsonify(message=message, **kwargs)
+
+@app.route("/")
+def get_index():
+    return json_success("Cuckoo Agent!")
+
+@app.route("/system")
+def get_system():
+    return json_success("System", system=platform.system())
+
+@app.route("/environ")
+def get_error():
+    return json_success("Environment variables", environ=os.environ)
+
+@app.route("/mkdir", methods=["POST"])
+def do_mkdir():
+    if "dirpath" not in request.form:
+        return json_error(400, "No dirpath has been provided")
+
+    mode = int(request.form.get("mode", 0777))
+
+    try:
+        os.makedirs(request.form["dirpath"], mode=mode)
+    except:
+        return json_exception("Error creating directory")
+
+    return json_success("Successfully created directory")
+
+@app.route("/mktemp", methods=["POST"])
+def do_mktemp():
+    suffix = request.form.get("suffix", "")
+    prefix = request.form.get("prefix", "tmp")
+    dirpath = request.form.get("dirpath")
+
+    fd, filepath = tempfile.mkstemp(suffix=suffix, prefix=prefix, dir=dirpath)
+    os.close(fd)
+
+    return json_success("Successfully created temporary file",
+                        filepath=filepath)
+
+@app.route("/mkdtemp", methods=["POST"])
+def do_mkdtemp():
+    suffix = request.form.get("suffix", "")
+    prefix = request.form.get("prefix", "tmp")
+    dirpath = request.form.get("dirpath")
+
+    dirpath = tempfile.mkdtemp(suffix=suffix, prefix=prefix, dir=dirpath)
+    return json_success("Successfully created temporary directory",
+                        dirpath=dirpath)
+
+@app.route("/store", methods=["POST"])
+def do_store():
+    if "filepath" not in request.form:
+        return json_error(400, "No filepath has been provided")
+
+    if "file" not in request.files:
+        return json_error(400, "No file has been provided")
+
+    try:
+        with open(request.form["filepath"], "rb") as f:
+            f.write(request.files["file"].read())
+    except:
+        return json_exception("Error storing file")
+
+@app.route("/extract", methods=["POST"])
+def do_extract():
+    if "dirpath" not in request.form:
+        return json_error(400, "No dirpath has been provided")
+
+    if "zipfile" not in request.files:
+        return json_error(400, "No zip file has been provided")
+
+    try:
+        with zipfile.ZipFile(request.files["zipfile"], "r") as archive:
+            archive.extractall(request.form["dirpath"])
+    except:
+        return json_exception("Error extracting zip file")
+
+    return json_success("Successfully extracted zip file")
+
+@app.route("/execute", methods=["POST"])
+def do_execute():
+    if "command" not in request.form:
+        return json_error(400, "No command has been provided")
+
+    try:
+        output = subprocess.check_output(request.form["command"], shell=True)
+    except:
+        return json_exception("Error executing command")
+
+    return json_success("Successfully executed command", output=output)
+
+@app.route("/kill")
+def do_kill():
+    os.unlink(__file__)
+    exit(0)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", nargs="?", default="0.0.0.0")
+    parser.add_argument("port", nargs="?", default="8000")
+    args = parser.parse_args()
+
+    app.run(host=args.host, port=int(args.port))
